a32a4faadab3760d34784b2a7bcf202751811f70
Fabien Fleutot <fabien@macfabien.local>
2008-01-04 18:55:56 +0100

    Added method pcall() and ability to call strings to springs.

--- src/lib/springs.lua
+++ src/lib/springs.lua
@@ -7,25 +7,50 @@ local rings_p_perms = { }
 local rings_u_perms = { }
 
 -- Serialize, send the request to the child state, deserialize the result
-local function call_send (r, ...)
-   local  msg_snd = pluto.persist (rings_p_perms, {...})
+local function pcall_send (r, f, ...)
+   local data
+   if type(f)=='string' then 
+      data = string.format (f, ...)
+   else
+      assert (type(f)=='function', "Springs can only call functions and strings")
+      data = { f, ... }
+   end
+   local  msg_snd = pluto.persist (rings_p_perms, data)
    local  st, msg_rcv = 
-      r:dostring (string.format ("return rings.call_receive %q", msg_snd))
+      r:dostring (string.format ("return rings.pcall_receive %q", msg_snd))
    if st then return unpack(pluto.unpersist (rings_u_perms, msg_rcv))
    else return st, msg_rcv end
 end
 
+local function call_send (r, f, ...)
+   local results = { r.pcall(r, f, ...) }
+   if results[1] then return select(2, unpack(results))
+   else error(results[2]) end
+end
+
 -- Receive a request from the master state, deserialize it, do it,
 -- serialize the result.
-local function call_receive (rcv_msg)
-   local args = pluto.unpersist (rings_u_perms, rcv_msg)
-   local r = { pcall (unpack (args)) }
-   return pluto.persist (rings_p_perms, r)
+local function pcall_receive (rcv_msg)
+   local data = pluto.unpersist (rings_u_perms, rcv_msg)
+   local result
+   if type(data) == 'string' then 
+      local f, msg = loadstring(data)
+      if not f then result = { false, msg }
+      else result = { pcall (f) } end
+   else
+      require 'std'
+      print("data received:")
+      table.print (data, 80)
+      assert (type(data)=='table', "illegal springs message")
+      result = { pcall (unpack (data)) }
+   end
+   return pluto.persist (rings_p_perms, result)
 end
 
 -- Monkey-patch rings
+debug.getregistry()['state metatable'].__index.pcall = pcall_send
 debug.getregistry()['state metatable'].__index.call = call_send
-rings.call_receive = call_receive
+rings.pcall_receive = pcall_receive
 
 local original_rings_new = rings.new
 function rings.new ()
