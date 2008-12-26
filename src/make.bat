93cb0a3b1dee87f73a52a6baacac5972c9868e8f
unknown <Fabien@.(none)>
2008-12-26 16:41:05 +0100

    rewrote the build and install system, tested on linux and windows

---
+++ src/make.bat
@@ -0,0 +1,55 @@
+@CLS
+
+@REM *** Settings ***
+
+@REM BASE        = root directory of metalua sources
+@REM DISTRIB_BIN = metalua executables target directory
+@REM DISTRIB_LIB = metalua libraries target directory, can be an existing path referenced in LUA_PATH
+@REM LUA, LUAC   = Lua executables, provided by metalua by default.
+
+@REM --- BEGINNING OF USER-EDITABLE PART ---
+
+@set BASE=%CD%
+@set DISTRIB=%BASE%\..\distrib
+@set DISTRIB_BIN=%DISTRIB%\bin
+@set DISTRIB_LIB=%DISTRIB%\lib
+@set LUA=%DISTRIB_BIN%\lua
+@set LUAC=%DISTRIB_BIN%\luac
+
+@REM --- END OF USER-EDITABLE PART ---
+
+
+@REM *** Create the distribution directories, populate them with lib sources ***
+
+mkdir %DISTRIB%
+mkdir %DISTRIB_BIN%
+mkdir %DISTRIB_LIB%
+xcopy /y /s lib %DISTRIB_LIB%
+xcopy /y /s bin %DISTRIB_BIN%
+
+@REM *** Generate a callable batch metalua.bat script ***
+
+echo @set LUA_PATH=?.luac;?.lua;%DISTRIB_LIB%\?.luac;%DISTRIB_LIB%\?.lua > %DISTRIB_BIN%\metalua.bat
+echo @set LUA_MPATH=?.mlua;%DISTRIB_LIB%\?.mlua >> %DISTRIB_BIN%\metalua.bat
+echo @%LUA% %DISTRIB_LIB%\metalua.luac %%* >> %DISTRIB_BIN%\metalua.bat
+
+
+@REM *** Compiling the parts of the compiler written in plain Lua ***
+
+cd compiler
+%LUAC% -o %DISTRIB_LIB%\metalua\bytecode.luac lopcodes.lua lcode.lua ldump.lua compile.lua
+%LUAC% -o %DISTRIB_LIB%\metalua\mlp.luac lexer.lua gg.lua mlp_lexer.lua mlp_misc.lua mlp_table.lua mlp_meta.lua mlp_expr.lua mlp_stat.lua mlp_ext.lua
+cd ..
+
+@REM *** Bootstrap the parts of the compiler written in metalua ***
+
+%LUA% %BASE%\build-utils\bootstrap.lua %BASE%\compiler\mlc.mlua output=%DISTRIB_LIB%\metalua\mlc.luac
+%LUA% %BASE%\build-utils\bootstrap.lua %BASE%\compiler\metalua.mlua output=%DISTRIB_LIB%\metalua.luac
+
+@REM *** Finish the bootstrap: recompile the metalua parts of the compiler with itself ***
+
+call %DISTRIB_BIN%\metalua -vb -f compiler\mlc.mlua     -o %DISTRIB_LIB%\metalua\mlc.luac
+call %DISTRIB_BIN%\metalua -vb -f compiler\metalua.mlua -o %DISTRIB_LIB%\metalua.luac
+
+@REM *** Precompile metalua libraries ***
+%LUA% %BASE%\build-utils\precompile.lua directory=%DISTRIB_LIB% command=%DISTRIB_BIN%\metalua
---
+++ src/make.sh
@@ -0,0 +1,96 @@
+# --- BEGINNING OF USER-EDITABLE PART ---
+
+# Metalua sources
+BASE=${PWD}
+
+# Temporary building location.
+# Upon installation, everything will be moved to ${INSTALL_LIB} and ${INSTALL_BIN}
+BUILD=/tmp/metalua-build
+BUILD_BIN=${BUILD}/bin
+BUILD_LIB=${BUILD}/lib
+
+# Where to place the final results
+INSTALL_BIN=/usr/local/bin
+INSTALL_LIB=/usr/local/lib/lua/5.1
+
+# Where to find Lua executables.
+# On many Debian-based systems, those can be installed with "sudo apt-get install lua5.1"
+LUA=$(which lua)
+LUAC=$(which luac)
+
+# --- END OF USER-EDITABLE PART ---
+
+
+echo '*** Lua paths setup ***'
+
+export LUA_PATH="?.luac;?.lua;${BUILD_LIB}/?.luac;${BUILD_LIB}/?.lua"
+export LUA_MPATH="?.mlua;${BUILD_LIB}/?.mlua"
+
+echo '*** Create the distribution directories, populate them with lib sources ***'
+
+mkdir -p ${BUILD_BIN}
+mkdir -p ${BUILD_LIB}
+cp -R lib/* ${BUILD_LIB}/
+cp -R bin/* ${BUILD_BIN}/
+
+echo '*** Generate a callable metalua shell script ***'
+
+cat > ${BUILD_BIN}/metalua <<EOF
+#!/bin/sh
+export LUA_PATH='?.luac;?.lua;${BUILD_LIB}/?.luac;${BUILD_LIB}/?.lua'
+export LUA_MPATH='?.mlua;${BUILD_LIB}/?.mlua'
+${LUA} ${BUILD_LIB}/metalua.luac \$*
+EOF
+chmod a+x ${BUILD_BIN}/metalua
+
+echo '*** Compiling the parts of the compiler written in plain Lua ***'
+
+cd compiler
+${LUAC} -o ${BUILD_LIB}/metalua/bytecode.luac lopcodes.lua lcode.lua ldump.lua compile.lua
+${LUAC} -o ${BUILD_LIB}/metalua/mlp.luac lexer.lua gg.lua mlp_lexer.lua mlp_misc.lua mlp_table.lua mlp_meta.lua mlp_expr.lua mlp_stat.lua mlp_ext.lua
+cd ..
+
+echo '*** Bootstrap the parts of the compiler written in metalua ***'
+
+${LUA} ${BASE}/build-utils/bootstrap.lua ${BASE}/compiler/mlc.mlua output=${BUILD_LIB}/metalua/mlc.luac
+${LUA} ${BASE}/build-utils/bootstrap.lua ${BASE}/compiler/metalua.mlua output=${BUILD_LIB}/metalua.luac
+
+echo '*** Finish the bootstrap: recompile the metalua parts of the compiler with itself ***'
+
+${BUILD_BIN}/metalua -vb -f compiler/mlc.mlua     -o ${BUILD_LIB}/metalua/mlc.luac
+${BUILD_BIN}/metalua -vb -f compiler/metalua.mlua -o ${BUILD_LIB}/metalua.luac
+
+echo '*** Precompile metalua libraries ***'
+for SRC in $(find ${BUILD_LIB} -name '*.mlua'); do
+    DST=$(dirname $SRC)/$(basename $SRC .mlua).luac
+    if [ $DST -nt $SRC ]; then
+        echo "+ $DST already up-to-date"
+    else
+        echo "- $DST generated from $SRC"
+        ${BUILD_BIN}/metalua $SRC -o $DST
+    fi
+done
+
+echo '*** Generate make-install.sh script ***'
+
+cat > make-install.sh <<EOF2
+#!/bin/sh
+mkdir -p ${INSTALL_BIN}
+mkdir -p ${INSTALL_LIB}
+
+cat > ${INSTALL_BIN}/metalua <<EOF
+#!/bin/sh
+export LUA_PATH='?.luac;?.lua;${INSTALL_LIB}/?.luac;${INSTALL_LIB}/?.lua'
+export LUA_MPATH='?.mlua;${INSTALL_LIB}/?.mlua'
+${LUA} ${INSTALL_LIB}/metalua.luac \$*
+EOF
+
+chmod a+x ${INSTALL_BIN}/metalua
+
+cp -R ${BUILD_LIB}/* ${INSTALL_LIB}/
+EOF2
+chmod a+x make-install.sh
+
+echo
+echo "Build completed, proceed to installation with './make-install.sh' or 'sudo ./make-install.sh'"
+echo
