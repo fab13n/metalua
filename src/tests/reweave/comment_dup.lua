6fe04c64ce4bbf58c2130036042b2e3412766d17
Alexander Gladysh <agladysh@gmail.com>
2009-02-09 01:23:59 +0300

    reweaving test suite based on actual bugs

---
+++ src/tests/reweave/comment_dup.lua
@@ -0,0 +1,8 @@
+if true then
+  -- comment
+end
+
+if true then
+  -- comment
+  print("something else after")
+end
---
+++ src/tests/reweave/function-index-decl.lua
@@ -0,0 +1,2 @@
+function a.b.c.f1 (x) return x+1 end
+function a.b.c:f2 (x,y) self[x] = y end
