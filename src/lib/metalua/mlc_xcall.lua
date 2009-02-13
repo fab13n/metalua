c7a29da029e83a62f6a93689bb86c073a8d609a0
Fabien Fleutot <metalua@gmail.com>
2009-02-13 13:50:55 +0100

    removed dead code from mlc_xcall

--- src/lib/metalua/mlc_xcall.lua
+++ src/lib/metalua/mlc_xcall.lua
@@ -1,20 +1,6 @@
--- lua -l mlc_xcall -e 'luafile_to_astfile ("/tmp/tmp12345.lua", "/tmp/tmp54321.ast")'
--- lua -l mlc_xcall -e 'lua_to_astfile ("/tmp/tmp54321.ast")'
+-- Does nothing: processes are simply run all in the same lua state for now.
 
 mlc_xcall = { }
-
-function mlc_xcall.server (luafilename, astfilename)
-
-   -- We don't want these to be loaded when people only do client-side business
-   require 'metalua.compiler'
-   require 'serialize'
-
-   -- compile the content of luafile name in an AST, serialized in astfilename
-   local ast = mlc.luafile_to_ast (luafilename)
-   local out = io.open (astfilename, 'w')
-   out:write (serialize (ast))
-   out:close ()
-end
 
 function mlc_xcall.client_file (luafilename)
    local ast = mlc.luafile_to_ast (luafilename)
