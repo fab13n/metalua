77931001400641316c4e7c23f603a5919529620f
Fabien Fleutot <fleutot@gmail.com>
2013-02-05 10:19:52 +0100

    fixed some bugs, implemented losy constraints collapsing

--- src/tilo.lua
+++ src/tilo.lua
@@ -9,7 +9,20 @@ function tilo(x)
     local gamma = gamma_new()
     local ts = typeof.sbar(gamma, x)
     gamma :close()
-    print(gamma :tostring())
+    print("\n"..gamma :tostring().."\n")
+
+    local sigma = gamma.te.eq :get_sigma()
+
+    if false then
+        local acc = { }
+        for k, v in pairs(sigma) do
+            table.insert(acc, string.format("%s->%s", k, a2s(v)))
+        end
+        local sigma_str = table.concat(acc, "; ")
+        printf("cmp.subst(%s, <<%s>>)", a2s(ts), sigma_str)
+    end
+
+    ts = cmp.subst(ts, sigma)
     print("Result: "..mlc.ast_to_src(ts))
     return ts
 end
