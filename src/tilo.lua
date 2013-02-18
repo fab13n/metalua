931ed4df14f1e90934770e26354d711cbfaf7447
Fabien Fleutot <fleutot@gmail.com>
2013-02-18 23:10:23 +0100

    heuristic tweaks

--- src/tilo.lua
+++ src/tilo.lua
@@ -8,11 +8,16 @@ function tilo(x)
     if type(x)=='string' then x = mlc.src_to_ast(x) end
     local gamma = gamma_new()
     local ts = typeof.sbar(gamma, x)
-    print("\n"..gamma :tostring().."\n")
-    gamma :close()
-    print("\n"..gamma :tostring().."\n")
 
+    print("\nRaw constraints:\n"..gamma :tostring().."\n")
+
+    gamma :close()
     local sigma = gamma.te.eq :get_sigma()
+    for name, cell in pairs(gamma.var_types) do
+        cell.type = cmp.subst(cell.type, sigma)
+    end
+
+    print("\nAfter heuristic simplifications:\n"..gamma :tostring().."\n")
 
     if false then
         local acc = { }
